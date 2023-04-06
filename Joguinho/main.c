2ee6f042bde1e243f0ee671efa83cd7dc9891395
Talescruzs <tcs2017@hotmail.com>
2023-04-06 19:14:44 -0300

    melhorando estrutura de decisao

--- Joguinho/main.c
+++ Joguinho/main.c
@@ -5,9 +5,7 @@
 struct Personagem{
     int vida;
     float ataque, velocidade;
-
-}padrao = {50, 3.5, 2};
-typedef struct Personagem Personagem_a;
+};
 
 //campo das funções:
 struct Personagem muda_vida(struct Personagem personagem, int valor_dano){
@@ -19,34 +17,29 @@ struct Personagem muda_vida(struct Personagem personagem, int valor_dano){
 int main()
 {
     // definindo personagens:
-    Personagem_a personagem1 = padrao;
-    Personagem_a personagem2 = {30, 5, 2};
+    struct Personagem personagem1 = {50, 3, 2};
+    struct Personagem personagem2 = {30, 5, 2};
 
     // batalha entre eles:
     int flag = 1;
     while(flag == 1){
-        switch(personagem1.vida<=0 && personagem2.vida<=0){
-            case 1:
+        if(personagem1.vida<=0 || personagem2.vida<=0){
+            if(personagem1.vida<=0 && personagem2.vida<=0){
                 printf("os dois morreram");
                 flag = 2;
-                break;
-            default:
-                if(personagem1.vida<=0){
-                    printf("personagem 1 morreu");
-                    flag = 2;
-                    break;
-                }
-                if(personagem2.vida<=0){
-                    printf("personagem 2 morreu");
-                    flag = 2;
-                    break;
-                }
-                printf("---------------------------\nBatalha rolando: \n\n\nDados da batalha:\n\n");
-                printf("Vida P1: %d \n", personagem1.vida);
-                printf("Vida P2: %d \n\n\n", personagem2.vida);
-                personagem1 = muda_vida(personagem1, personagem2.ataque*-1);
-                personagem2 = muda_vida(personagem2, personagem1.ataque*-1);
-
+            }else if(personagem1.vida<=0){
+                printf("personagem 1 morreu");
+                flag = 2;
+            }else{
+                printf("personagem 2 morreu");
+                flag = 2;
+            }
+        }else{
+            printf("---------------------------\nBatalha rolando: \n\n\nDados da batalha:\n\n");
+            printf("Vida P1: %d \n", personagem1.vida);
+            printf("Vida P2: %d \n\n\n", personagem2.vida);
+            personagem1 = muda_vida(personagem1, personagem2.ataque*-1);
+            personagem2 = muda_vida(personagem2, personagem1.ataque*-1);
         }
     }
 
